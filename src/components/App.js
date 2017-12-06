89779309c27153f074564ad0e78197ee391762b8
Nacho Martín <nacho@limenius.com>
2017-12-06 21:04:53 +0100

    Some styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ const React = require("react");
 const Breadcrumb = require("./Breadcrumb");
 const Sidebar = require("./Sidebar");
 const Container = require("./atoms/Container");
+const ContentContainer = require("./atoms/ContentContainer");
 const LeftBar = require("./atoms/LeftBar");
 const Wide = require("./atoms/Wide");
 
 const App = ({ children, breadcrumb, sidebar }) => (
   <Container>
     <Wide>
-      <Breadcrumb breadcrumb={breadcrumb} />
-      <div>{children}</div>
+      <ContentContainer>
+        <Breadcrumb breadcrumb={breadcrumb} />
+        <div>{children}</div>
+      </ContentContainer>
     </Wide>
     <LeftBar>
       <Sidebar tree={sidebar} />
---
+++ src/components/atoms/ContentContainer.js
@@ -0,0 +1,7 @@
+const styled = require("styled-components").default;
+
+module.exports = styled.div`
+margin: 0 auto;
+padding: 1em;
+max-width: 960px;
+`
